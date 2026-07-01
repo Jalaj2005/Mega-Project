6e24898262c7f40a9db5584f4d3a60b6b08e8efe
Jalaj2005 <jalajsingh2005@gmail.com>
2026-07-02 00:07:41 +0530

    add listings owner

--- init/index.js
+++ init/index.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const intiData =  require("./data.js");
 const Listing = require("../models/listing.js");
+const initData = require("./data.js");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
@@ -17,7 +18,7 @@ async function main() {
 
 const initDB = async () => {
     await Listing.deleteMany({});
-    initData.data.map((obj) => ({...obj}));
+    initData.data.map((obj) => ({...obj, owner: "6a3f5aa6e3cea8846fd9bbdd"}));
     await Listing.insertMany(intiData.data);
     console.log("data was initialized");
 };
--- routes/listing.js
+++ routes/listing.js
@@ -34,11 +34,12 @@ router.get("/new", isLoggedIn, (req, res) => { //isLoggedIn acts as middleware o
 //Show Route
 router.get("/:id", wrapAsync(async (req, res) => {
     let { id } = req.params;
-    const listing = await Listing.findById(id).populate("reviews");
+    const listing = await Listing.findById(id).populate("reviews").populate("owner");
     if (!listing) {
         req.flash("error", "Listing you requested for does not exist!");
         return res.redirect("/listings")
     }
+    console.log(listing);
     res.render("listings/show.ejs", { listing });
 }));
 
@@ -49,12 +50,8 @@ router.post(
     isLoggedIn,
     validateListing,
     wrapAsync(async (req, res, next) => {
-        let result = listingSchema.validate(req.body);
-        console.log(result);
-        if (result.error) {
-            throw new ExpressError(400, result.error);
-        }
         const newListing = new Listing(req.body.listing);
+        newListings.owner = req.user._id;
         await newListing.save();
         req.flash("success", "New listing created!");
         res.redirect("/listings");
